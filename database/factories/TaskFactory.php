21fece7d6010ac814f408a608c101a410c4d62bc
Néstor <nestor@parqueweb.com>
2023-07-05 17:49:30 +0200

    feat(TaskFactory): HTML description

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -23,9 +23,30 @@ class TaskFactory extends Factory
         return [
             'user_id' => User::all()->random()->id,
             'title' => fake()->sentence(3),
-            'description' => fake()->paragraph(13),
+            // 'description' => fake()->paragraph(13),
+            'description' => str_replace(['<html>', '<body>', '</body>', '</html>'], '',
+                $this->removeHtmlTagsAndContent(
+                    fake()->randomHtml(6, 6),
+                    ['head', 'script', 'style', 'form']
+                ),
+            ),
             'is_completed' => $is_completed,
             'completed_at' => $completed_at,
         ];
+    }
+
+    public function removeHtmlTagsAndContent($html, $tags)
+    {
+        $dom = new \DOMDocument();
+        @$dom->loadHTML($html, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+        $xpath = new \DOMXPath($dom);
+
+        foreach ($tags as $tag) {
+            while ($node = $xpath->query("//{$tag}")->item(0)) {
+                $node->parentNode->removeChild($node);
+            }
+        }
+
+        return $dom->saveHTML();
     }
 }
