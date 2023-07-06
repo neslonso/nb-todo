1389985592e2bc1f895593a70c1ddf33bd3720df
Néstor <nestor@parqueweb.com>
2023-07-06 16:07:04 +0200

    feat: route for serving vue3-client

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+ * Servimos la aplicación Vue.js si no nos piden una ruta de la API o Telescope
+ *
+ * OJO: Lo suyo sería servir la aplicación Vue.js desde un servidor web (e.g Nginx o Apache)
+ * distinto al de la API y sin Laravel, pero para simplificar el ejemplo lo hacemos desde Laravel.
+ */
+Route::get('/{any?}', function ($any = null) {
+    $path = public_path('vue3-client/'.$any);
+
+    if ($any && file_exists($path)) {
+        if (pathinfo($path, PATHINFO_EXTENSION) == 'css') {
+            return response(file_get_contents($path))->header('Content-Type', 'text/css');
+        } elseif (pathinfo($path, PATHINFO_EXTENSION) == 'js') {
+            return response(file_get_contents($path))->header('Content-Type', 'application/javascript');
+        } else {
+            return file_get_contents($path);
+        }
+    } else {
+        return file_get_contents(public_path('vue3-client/index.html'));
+    }
+})->where('any', '^(?!sanctum|api|telescope).*$');
